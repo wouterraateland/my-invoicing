245906f63af3487e91f1f5273cb32574d0fe2d9d
Wouter Raateland <wouterraateland@gmail.com>
2018-12-19 02:26:47 +0100

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import './styles.css'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from 'components/App'
+
+const Root = (
+  <App />
+)
+
+const root = document.getElementById('root')
+
+render(Root, root)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
